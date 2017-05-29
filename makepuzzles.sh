275ddc545238812d395302d9798a2b0dc5587000
minimart64 <minimart@me.com>
2017-05-29 07:50:27 -0400

    took file delete out of cnee and put a move in bash instead

--- makepuzzles.sh
+++ makepuzzles.sh
@@ -2,9 +2,11 @@
 
 # count of files in ~/Documents/Photos
 # loop to repeat cnee call that many times
+# cnee creates the puzzle and then copy-pastes the url into Geany
 
-for i in $(ls ~/Documents/Photos); do
-cnee --replay --file cneeScript.xns -force-core-replay
+cd ~/Documents/Photos
+for i in $(ls); do
+cnee --replay --file ~/Documents/git/Jigidize/cneeScript.xns -force-core-replay
 mv $i ~/Documents/cneed/
 done
 
