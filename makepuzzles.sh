2a30bf8788e186fc4cc4eb5de825da9624b97fb6
minimart64 <minimart@me.com>
2017-06-07 22:01:59 -0400

    shifted the moving of puzzle codes to jigidize.py, no longer part of cnee

--- makepuzzles.sh
+++ makepuzzles.sh
@@ -2,11 +2,15 @@
 
 # count of files in ~/Documents/Photos
 # loop to repeat cnee call that many times
-# cnee creates the puzzle and then copy-pastes the url into Geany
+# cnee creates the puzzles
+# jigidize then adds the new puzzle codes to the list (non-publish)
 
 cd ~/Documents/Photos
+declare -i count; count=0
 for i in $(ls); do
 cnee --replay --file ~/Documents/git/Jigidize/cneeScript.xns -force-core-replay
 mv $i ~/Documents/cneed/
+count+=1
+echo $count
 done
-
+~/Documents/git/Jigidize/jigidize.py -x $count
