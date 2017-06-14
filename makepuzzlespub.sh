f10acfbab016608777135837150164deb10036fe
minimart64 <minimart@me.com>
2017-06-13 21:49:12 -0400

    added a call to jigidize after 24 puzzles to avoid the page limit on my puzzles

--- makepuzzlespub.sh
+++ makepuzzlespub.sh
@@ -9,11 +9,17 @@
 cd ~/Documents/PhotosPublic
 declare -i count; count=0
 for i in $(ls); do
-mv $i ~/Documents/cneeing/
-cnee --replay --file ~/Documents/git/Jigidize/cneeScript.xns -force-core-replay
-mv ~/Documents/cneeing/$i ~/Documents/cneed/
-count+=1
-echo $count
+    mv $i ~/Documents/cneeing/
+    cnee --replay --file ~/Documents/git/Jigidize/cneeScript.xns -force-core-replay
+    mv ~/Documents/cneeing/$i ~/Documents/cneed/
+    count+=1
+    echo $count
+    # if we have made 24 puzzles, stop to get them from jigid before we make more
+    if ((count==24)); then
+        echo "Jigidizing 24"
+        ~/Documents/git/Jigidize/jigidize.py -xp 24
+        count=0
+    fi
 done
 
 echo "Jigidizing"
